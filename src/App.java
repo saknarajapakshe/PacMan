9c79019d7ee4c15b3285e1a70aeb69db911d4389
saknarajapakshe <rajapaksherars.22@uom.lk>
2025-02-06 16:56:28 +0530

    Initialize Java project with VS Code settings, README, and main application class

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+        //frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(frame);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacManGame = new PacMan();
+        frame.add(pacManGame);
+        frame.pack();
+        pacManGame.requestFocus();
+        frame.setVisible(true);   
+    }
+}
